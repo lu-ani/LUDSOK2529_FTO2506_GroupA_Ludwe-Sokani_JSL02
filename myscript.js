effc47a1380b3489e457405b148d2e0a02e10bba
Ludwe Sokani <ludwesokani@gmail.com>
2025-08-31 14:45:39 +0200

    Check if TaskStatus1 is correct using while loop

--- myscript.js
+++ myscript.js
@@ -5,3 +5,15 @@ let TaskTitle1 = prompt("Enter Task 1 title: ");
 let TaskDetails1 = prompt("Enter the Task 1 description: ");
 
 let TaskStatus1 = prompt("Enter Task 1 status: ").toLowerCase(); //convert the status to lowercase for checks
+
+// check if the task status is valid and if not, prompt user until it is
+
+while (
+  TaskStatus1 !== "todo" &&
+  TaskStatus1 !== "doing" &&
+  TaskStatus1 !== "done"
+) {
+  TaskStatus1 = prompt(
+    "Enter Task 1 Status: (todo, doing or done)"
+  ).toLowerCase();
+}
