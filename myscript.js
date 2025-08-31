ed1e5668b3e0d07cc66a9821eb4eda3499db5f71
Ludwe Sokani <ludwesokani@gmail.com>
2025-08-31 14:44:48 +0200

    Prompt user for variable values

--- myscript.js
+++ myscript.js
@@ -1,1 +1,7 @@
+// initialize by prompting variables outside loop
 
+let TaskTitle1 = prompt("Enter Task 1 title: ");
+
+let TaskDetails1 = prompt("Enter the Task 1 description: ");
+
+let TaskStatus1 = prompt("Enter Task 1 status: ").toLowerCase(); //convert the status to lowercase for checks
